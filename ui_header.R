4e6cfb7bd306e166f468249fe2b2640afb920c0b
MariosCh1 <mmch07@gmail.com>
2022-05-22 21:28:04 +0300

    Logo Issues

--- ui_header.R
+++ ui_header.R
@@ -1,1 +1,7 @@
-header <- shinydashboardPlus::dashboardHeader(title = img(src='logo.png'), userOutput("user"))
+header <- shinydashboardPlus::dashboardHeader(title = tags$img(src="https://github.com/MariosCh1/Interactive-Web-Application-to-Deploy-Machine-Learning-Models-using-R-Shiny/blob/a8f98150e93fb256786aa4dd9b527a9f11dba9b4/www/logo.png?raw=true", 
+                                                               height = '40', width='101',
+                                                               tags$style(
+                                                                 HTML(
+                                                                   " .main-header .logo {padding: 0 8px;}"
+                                                                 )
+                                                               )), userOutput("user"))
