52499d8890036195c3081439aab85bfbad027152
MariosCh1 <mmch07@gmail.com>
2022-05-22 11:55:19 +0300

    Logo File Adjustments

--- ui_header.R
+++ ui_header.R
@@ -1,1 +1,1 @@
-header <- shinydashboardPlus::dashboardHeader(title = tags$a(tags$img(src = "logo.png"), align = "center"), userOutput("user"))
+header <- shinydashboardPlus::dashboardHeader(title = img(src='logo.png'), userOutput("user"))
