ddaa26947bcd7c3cc4998edba2d6cf83d9b678c5
MariosCh1 <mmch07@gmail.com>
2022-05-22 21:40:44 +0300

    CSS Logo Adjustments

--- ui_header.R
+++ ui_header.R
@@ -1,7 +1,7 @@
-header <- shinydashboardPlus::dashboardHeader(title = tags$img(src="https://github.com/MariosCh1/Interactive-Web-Application-to-Deploy-Machine-Learning-Models-using-R-Shiny/blob/a8f98150e93fb256786aa4dd9b527a9f11dba9b4/www/logo.png?raw=true", 
-                                                               height = '40', width='101',
+header <- shinydashboardPlus::dashboardHeader(title = tags$img(src="https://github.com/MariosCh1/Interactive-Web-Application-to-Deploy-Machine-Learning-Models-using-R-Shiny/blob/main/www/logo.png?raw=true", 
+                                                               height = '40', width='114',
                                                                tags$style(
                                                                  HTML(
-                                                                   " .main-header .logo {padding: 0 8px;}"
+                                                                   " .main-header .logo {padding: 0 7px;}"
                                                                  )
                                                                )), userOutput("user"))
