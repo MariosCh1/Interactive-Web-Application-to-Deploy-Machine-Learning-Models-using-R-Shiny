fdf9e6640ad73f022aa937f2fc4b2f9c3331f0b6
MariosCh1 <mmch07@gmail.com>
2023-02-21 08:20:39 +0200

    Logo Changing

--- ui_header.R
+++ ui_header.R
@@ -1,10 +1,10 @@
 header <- shinydashboardPlus::dashboardHeader(
   
   title = tags$img(
-    src = "https://raw.githubusercontent.com/MariosCh1/Interactive-Web-Application-to-Deploy-Machine-Learning-Models-using-R-Shiny/main/www/logo.png",
+    src = "https://raw.githubusercontent.com/MariosCh1/Interactive-Web-Application-to-Deploy-Machine-Learning-Models-using-R-Shiny/main/www/logo_white.png",
     height = '40',
     width = '114',
-    tags$style(HTML(" .main-header .logo {padding: 0 7px;}"))
+    tags$style(HTML(" .main-header .logo {padding: 0 8px;}"))
   ),
   userOutput("user"),
   controlbarIcon = icon("bars")
